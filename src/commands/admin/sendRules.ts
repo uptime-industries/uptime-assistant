50146fe1f86b9ab40b42565899fc828089c09b2a
John William Bowen <johnwb1776@gmail.com>
2023-04-17 20:49:56 -0400

    Remove self roles

--- src/commands/admin/sendRules.ts
+++ src/commands/admin/sendRules.ts
@@ -17,23 +17,18 @@ const command:ChatInputCommand = {
     async execute(client, interaction) {
         const channel = interaction.options.getChannel('channel', true);
         if (!(channel instanceof TextChannel)) { return; }
-        await channel.send('https://cdn.discordapp.com/attachments/1014458643816661083/1073779941252022313/b681a17ca10c3fa31c05fa2b440e3640.png');
         await channel.send({
             embeds:[new EmbedBuilder()
                 .setTitle('Welcome to the Uptime Lab Discord')
                 .setDescription(fallback('welcome'))
-                .setColor(client.config.colors.embed)],
+                .setColor(client.config.colors.embed)
+                .setImage('https://cdn.discordapp.com/attachments/1014458643816661083/1073779941252022313/b681a17ca10c3fa31c05fa2b440e3640.png')],
             components:[new ActionRowBuilder<MessageActionRowComponentBuilder>()
                 .addComponents(new ButtonBuilder()
                     .setCustomId('rules')
                     .setLabel('Rules')
                     .setEmoji('📜')
                     .setStyle(ButtonStyle.Primary))
-                .addComponents(new ButtonBuilder()
-                    .setCustomId('roles_skill')
-                    .setLabel('Roles')
-                    .setEmoji('🛠️')
-                    .setStyle(ButtonStyle.Success))
                 .addComponents(new ButtonBuilder()
                     .setCustomId('socials')
                     .setLabel('Socials')
